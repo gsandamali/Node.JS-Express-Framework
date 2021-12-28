25e4568d38aca6cfde4007595747228f6482c1e9
Geethika Sandamali <86560608+gsandamali@users.noreply.github.com>
2021-12-28 09:45:18 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name:'gigi',
+        email:'gigi@gmail.com'
+    },
+    {
+        id: 2,
+        name:'sada',
+        email:'sada@gmail.com'
+    },
+    {
+        id: 3,
+        name:'sahan',
+        email:'sahan@gmail.com'
+    },
+    {
+        id: 4,
+        name:'wick',
+        email:'wick@gmail.com'
+    }
+];
+
+module.exports = users;
